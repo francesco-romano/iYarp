71da101d14d9ee0014e9e7d39e75a05b87f1e2e5
Francesco Romano <francesco.romano.1987@gmail.com>
2015-07-19 18:13:21 +0200

    Added IOL skeleton application

---
+++ apps/IOL/IOL/FirstViewController.h
@@ -0,0 +1,15 @@
+//
+//  FirstViewController.h
+//  IOL
+//
+//  Created by Francesco Romano on 19/07/15.
+//  Copyright (c) 2015 Francesco Romano. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FirstViewController : UIViewController
+
+
+@end
+
