993cb1242df7e2007736c13c8c0aa476f8b371b9
Francesco Romano <francesco.romano.1987@gmail.com>
2015-07-24 18:26:19 +0200

    Working on yarp write

--- libs/yarp_iOS/yarp_iOS/yarp write/IITYarpWrite.h
+++ libs/yarp_iOS/yarp_iOS/yarp write/IITYarpWrite.h
@@ -9,9 +9,15 @@
 #import <Foundation/Foundation.h>
 
 @interface IITYarpWrite : NSObject
-@property (nonatomic, readonly, strong) NSString *sourcePortName;
+@property (nonatomic, readonly, strong) NSString *writePortName;
 @property (nonatomic, readonly, strong) NSString *destinationPortName;
 @property (nonatomic, readonly, getter=isConnected) BOOL connected;
+
+- (BOOL)openPortNamed:(NSString*)portName;
+- (void)closePort;
+
+- (BOOL)connectToDestinationPortNamed:(NSString*)destinationPortName;
+- (BOOL)disconnectPort;
 
 - (void)write: (NSDictionary*)data;
 - (void)write: (NSDictionary*)data blocking:(BOOL)blocking;
--- libs/yarp_iOS/yarp_iOS/yarp write/IITYarpWrite.mm
+++ libs/yarp_iOS/yarp_iOS/yarp write/IITYarpWrite.mm
@@ -10,22 +10,71 @@
 #import "NSDictionary+Bottle.h"
 #import <yarp/os/BufferedPort.h>
 #import <yarp/os/Bottle.h>
+#import <yarp/os/Network.h>
 
 @interface IITYarpWrite () {
     yarp::os::BufferedPort<yarp::os::Bottle> *_outputPort;
 }
+@property (nonatomic, readwrite, strong) NSString *writePortName;
+@property (nonatomic, readwrite, strong) NSString *destinationPortName;
 
 @end
 
 @implementation IITYarpWrite
 
+- (BOOL)openPortNamed:(NSString*)portName
+{
+    if (_outputPort) {
+        [self closePort];
+    }
+    if (![portName length]) return NO;
+    self.writePortName = portName;
 
-- (void)write: (NSDictionary*)data
+    _outputPort = new yarp::os::BufferedPort<yarp::os::Bottle>();
+    if (!_outputPort) return NO;
+
+    return _outputPort->open([portName UTF8String]) ? YES : NO;
+}
+
+- (void)closePort
+{
+    if (_outputPort && !_outputPort->isClosed()) {
+        _outputPort->close();
+        delete _outputPort;
+        _outputPort = 0;
+    }
+}
+
+- (BOOL)isConnected
+{
+    if (!_outputPort) return NO;
+    if (_outputPort->isClosed()) return NO;
+    //???
+    return YES;
+}
+
+- (BOOL)connectToDestinationPortNamed:(NSString*)destinationPortName
+{
+    if (!_outputPort || _outputPort->isClosed()) return NO;
+    self.destinationPortName = destinationPortName;
+
+    using namespace yarp::os;
+    return Network::connect([self.writePortName UTF8String], [self.destinationPortName UTF8String]) ? YES : NO;
+}
+
+- (BOOL)disconnectPort
+{
+    using namespace yarp::os;
+
+    return Network::disconnect([self.writePortName UTF8String], [self.destinationPortName UTF8String]) ? YES : NO;
+}
+
+- (void)write:(NSDictionary*)data
 {
     [self write:data blocking:NO];
 }
 
-- (void)write: (NSDictionary*)data blocking:(BOOL)blocking
+- (void)write:(NSDictionary*)data blocking:(BOOL)blocking
 {
     using namespace yarp::os;
 
