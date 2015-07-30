8c4bd88e4136327fe96fe5474da511710f4ae5a3
Francesco Romano <francesco.romano.1987@gmail.com>
2015-07-30 17:59:15 +0200

    Working on camera acquisition

---
+++ apps/IOL/IOL/ViewControllers/FrameCaptureViewController.h
@@ -0,0 +1,13 @@
+//
+//  FrameCaptureViewController.h
+//  IOL
+//
+//  Created by Francesco Romano on 30/07/15.
+//  Copyright (c) 2015 Francesco Romano. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FrameCaptureViewController : UIViewController
+
+@end
---
+++ apps/IOL/IOL/ViewControllers/FrameCaptureViewController.m
@@ -0,0 +1,159 @@
+//
+//  FrameCaptureViewController.m
+//  IOL
+//
+//  Created by Francesco Romano on 30/07/15.
+//  Copyright (c) 2015 Francesco Romano. All rights reserved.
+//
+
+#import "FrameCaptureViewController.h"
+#import <AVFoundation/AVFoundation.h>
+
+@interface FrameCaptureViewController () <AVCaptureVideoDataOutputSampleBufferDelegate>
+@property (nonatomic, weak) IBOutlet UIView *previewView;
+@property (nonatomic, strong) AVCaptureSession *captureSession;
+@property (nonatomic, strong) AVCaptureVideoPreviewLayer *previewLayer;
+@property (nonatomic) BOOL cameraAuthorized;
+@end
+
+@implementation FrameCaptureViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+
+    //    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_HIGH, 0), ^{
+
+    NSError *error = nil;
+    self.cameraAuthorized = NO;
+
+    // Create the session
+    AVCaptureSession *session = [[AVCaptureSession alloc] init];
+
+    // Configure the session to produce lower resolution video frames, if your
+    // processing algorithm can cope. We'll specify medium quality for the
+    // chosen device.
+    session.sessionPreset = AVCaptureSessionPresetMedium;
+
+    // Find a suitable AVCaptureDevice
+    AVCaptureDevice *device = [AVCaptureDevice
+                               defaultDeviceWithMediaType:AVMediaTypeVideo];
+
+    // Create a device input with the device and add it to the session.
+    AVCaptureDeviceInput *input = [AVCaptureDeviceInput deviceInputWithDevice:device
+                                                                        error:&error];
+    if (!input) {
+        // Handling the error appropriately.
+    }
+    [session addInput:input];
+
+    // Create a VideoDataOutput and add it to the session
+    AVCaptureVideoDataOutput *output = [[AVCaptureVideoDataOutput alloc] init];
+    [session addOutput:output];
+
+    // Configure your output.
+    dispatch_queue_t queue = dispatch_queue_create("it.iit.iol.queue.videocapture", DISPATCH_QUEUE_SERIAL);
+    [output setSampleBufferDelegate:self queue:queue];
+
+    // Specify the pixel format
+    output.videoSettings =
+    [NSDictionary dictionaryWithObject:
+     [NSNumber numberWithInt:kCVPixelFormatType_32BGRA]
+                                forKey:(id)kCVPixelBufferPixelFormatTypeKey];
+
+
+    self.previewLayer = [[AVCaptureVideoPreviewLayer alloc] initWithSession:session];
+    self.previewLayer.videoGravity = AVLayerVideoGravityResizeAspect;
+    self.previewLayer.frame = self.view.bounds;
+    [self.previewLayer setBackgroundColor:[[UIColor blackColor] CGColor]];
+    [self.view.layer addSublayer:self.previewLayer];
+
+    self.captureSession = session;
+
+    [AVCaptureDevice requestAccessForMediaType:AVMediaTypeVideo completionHandler:^(BOOL granted) {
+        self.cameraAuthorized = granted;
+        if (!granted)
+        {
+            //Not granted access to mediaType
+            dispatch_async(dispatch_get_main_queue(), ^{
+                //migrate to new class
+                [[[UIAlertView alloc] initWithTitle:@"AVCam!"
+                                            message:@"AVCam doesn't have permission to use Camera, please change privacy settings"
+                                           delegate:self
+                                  cancelButtonTitle:@"OK"
+                                  otherButtonTitles:nil] show];
+            });
+        } else {
+            dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_HIGH, 0), ^{
+                [self.captureSession startRunning];
+            });
+        }
+    }];
+
+    //    });
+}
+
+- (void)captureOutput:(AVCaptureOutput *)captureOutput
+didOutputSampleBuffer:(CMSampleBufferRef)sampleBuffer
+       fromConnection:(AVCaptureConnection *)connection
+{
+    NSLog(@"Frame");
+}
+
+- (void)viewDidAppear:(BOOL)animated
+{
+    [super viewDidAppear:animated];
+    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_HIGH, 0), ^{
+        if (![self.captureSession isRunning])
+            [self.captureSession startRunning];
+    });
+}
+
+- (void)viewWillDisappear:(BOOL)animated
+{
+    [super viewWillDisappear:animated];
+    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_HIGH, 0), ^{
+        [self.captureSession stopRunning];
+    });
+}
+
+- (UIImage *)imageFromSampleBuffer:(CMSampleBufferRef) sampleBuffer
+{
+    // Get a CMSampleBuffer's Core Video image buffer for the media data
+    CVImageBufferRef imageBuffer = CMSampleBufferGetImageBuffer(sampleBuffer);
+    // Lock the base address of the pixel buffer
+    CVPixelBufferLockBaseAddress(imageBuffer, 0);
+
+    // Get the number of bytes per row for the pixel buffer
+    void *baseAddress = CVPixelBufferGetBaseAddress(imageBuffer);
+
+    // Get the number of bytes per row for the pixel buffer
+    size_t bytesPerRow = CVPixelBufferGetBytesPerRow(imageBuffer);
+    // Get the pixel buffer width and height
+    size_t width = CVPixelBufferGetWidth(imageBuffer);
+    size_t height = CVPixelBufferGetHeight(imageBuffer);
+
+    // Create a device-dependent RGB color space
+    CGColorSpaceRef colorSpace = CGColorSpaceCreateDeviceRGB();
+
+    // Create a bitmap graphics context with the sample buffer data
+    CGContextRef context = CGBitmapContextCreate(baseAddress, width, height, 8,
+                                                 bytesPerRow, colorSpace, kCGBitmapByteOrder32Little | kCGImageAlphaPremultipliedFirst);
+    // Create a Quartz image from the pixel data in the bitmap graphics context
+    CGImageRef quartzImage = CGBitmapContextCreateImage(context);
+    // Unlock the pixel buffer
+    CVPixelBufferUnlockBaseAddress(imageBuffer,0);
+
+    // Free up the context and color space
+    CGContextRelease(context);
+    CGColorSpaceRelease(colorSpace);
+
+    // Create an image object from the Quartz image
+    UIImage *image = [UIImage imageWithCGImage:quartzImage];
+
+    // Release the Quartz image
+    CGImageRelease(quartzImage);
+    
+    return (image);
+}
+
+@end
